34840064a5503456a607485182d4a3e1f522dcab
nemanjar00 <149513009+nemanjar00@users.noreply.github.com>
2024-05-28 10:33:30 +0200

    Ispravljeno mapiranje za objekat

--- Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/Objekat.cs
+++ Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/Objekat.cs
@@ -1,18 +1,7 @@
 ﻿namespace ZelenaPovrsina.Entiteti
 {
-    public class Objekat
+    public abstract class Objekat
     {
         public virtual required ObjekatId Id { get; set; }
-
-        public virtual string TipZO { get; set; }
-        public virtual int IdZastite { get; set; }
-        public virtual string NazivSpom { get; set; }
-        public virtual string AutorSkul { get; set; }
-        public virtual int IdParka { get; set; }  //ne znam da li treba ovo
-        public virtual int RedniBr { get; set; } //ne znam da li treba ovo
-
-        public virtual Park Park { get; set; }
-
-        
     }
 }
--- Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/ObjekatId.cs
+++ Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/ObjekatId.cs
@@ -2,5 +2,25 @@
 {
     public class ObjekatId
     {
+        public virtual Park? Park { get; set; }
+        public virtual int RedniBr { get; set; }
+
+        #region Overrides   
+        public override bool Equals(object? obj)
+        {
+            if(obj == null) 
+                return false;
+            if(ReferenceEquals(this,obj))  
+                return true;
+            ObjekatId compare = (obj as ObjekatId)!;
+            return Park!.IdZ == compare!.Park!.IdZ && RedniBr == compare.RedniBr;
+        }
+
+        public override int GetHashCode()
+        {
+            return Park!.IdZ.GetHashCode();
+        }
+
+        #endregion
     }
 }
