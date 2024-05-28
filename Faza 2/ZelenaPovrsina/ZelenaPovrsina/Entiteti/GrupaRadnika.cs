75cf30aef5aaba0aecfb0af232980b349e4520c0
nemanjar00 <149513009+nemanjar00@users.noreply.github.com>
2024-05-29 00:43:23 +0200

    opet

--- Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/GrupaRadnika.cs
+++ Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/GrupaRadnika.cs
@@ -3,11 +3,11 @@
     public class GrupaRadnika
     {
         public virtual int IdG { get; protected set; }
-        public required virtual string NazivG { get; set; }
+        public  virtual string NazivG { get; set; }
         
 
-        public required virtual Park Park { get; set; } //zbog one to one se pise
-        public required virtual Sef Sef { get; set; }
+        public  virtual Park Park { get; set; } //zbog one to one se pise
+        public  virtual Sef Sef { get; set; }
         public virtual IList<Radnik> RadniciGrupe { get; set; } = [];
 
         //public GrupaRadnika() { 
--- Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/Radnik.cs
+++ Faza 2/ZelenaPovrsina/ZelenaPovrsina/Entiteti/Radnik.cs
@@ -3,10 +3,10 @@
     public class Radnik
     {
         public virtual int IdR { get; protected set; }
-        public required virtual string Ime { get; set; }
-        public required virtual string Prezime { get; set; }
-        public required virtual string Jmbg { get; set; }
-        public required virtual string Adresa { get; set; }
+        public  virtual string Ime { get; set; }
+        public  virtual string Prezime { get; set; }
+        public  virtual string Jmbg { get; set; }
+        public  virtual string Adresa { get; set; }
         public virtual int BrRadneKnjizice { get; set; }
         public virtual string? ImeRoditelja { get; set; }
         public virtual string? StrucnaSprema { get; set; }
