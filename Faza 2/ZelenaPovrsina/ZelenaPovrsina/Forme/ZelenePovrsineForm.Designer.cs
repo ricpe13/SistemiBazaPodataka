a48322a488704826d6cd95be969d48d906a2dc65
ricpe13 <pericpavle002@gmail.com>
2024-05-30 00:53:28 +0200

    Za pokretanje PocetnaForm

--- Faza 2/ZelenaPovrsina/ZelenaPovrsina/Forme/ZelenePovrsineForm.Designer.cs
+++ Faza 2/ZelenaPovrsina/ZelenaPovrsina/Forme/ZelenePovrsineForm.Designer.cs
@@ -34,9 +34,6 @@
             columnHeader2 = new ColumnHeader();
             columnHeader3 = new ColumnHeader();
             columnHeader4 = new ColumnHeader();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
             button4 = new Button();
             button5 = new Button();
             groupBox1.SuspendLayout();
@@ -45,11 +42,9 @@
             // groupBox1
             // 
             groupBox1.Controls.Add(listView1);
-            groupBox1.Location = new Point(17, 20);
-            groupBox1.Margin = new Padding(4, 5, 4, 5);
+            groupBox1.Location = new Point(12, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new Padding(4, 5, 4, 5);
-            groupBox1.Size = new Size(537, 1067);
+            groupBox1.Size = new Size(376, 640);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Lista zelenih povrsina";
@@ -59,10 +54,9 @@
             listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
             listView1.FullRowSelect = true;
             listView1.GridLines = true;
-            listView1.Location = new Point(9, 37);
-            listView1.Margin = new Padding(4, 5, 4, 5);
+            listView1.Location = new Point(6, 22);
             listView1.Name = "listView1";
-            listView1.Size = new Size(508, 1017);
+            listView1.Size = new Size(357, 612);
             listView1.TabIndex = 0;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
@@ -89,43 +83,11 @@
             columnHeader4.TextAlign = HorizontalAlignment.Center;
             columnHeader4.Width = 80;
             // 
-            // button1
-            // 
-            button1.Location = new Point(697, 432);
-            button1.Margin = new Padding(4, 5, 4, 5);
-            button1.Name = "button1";
-            button1.Size = new Size(230, 63);
-            button1.TabIndex = 1;
-            button1.Text = "Parkovi";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(697, 545);
-            button2.Margin = new Padding(4, 5, 4, 5);
-            button2.Name = "button2";
-            button2.Size = new Size(230, 60);
-            button2.TabIndex = 2;
-            button2.Text = "Drvoredi";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(697, 657);
-            button3.Margin = new Padding(4, 5, 4, 5);
-            button3.Name = "button3";
-            button3.Size = new Size(230, 62);
-            button3.TabIndex = 3;
-            button3.Text = "Travnjaci";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
-            // 
             // button4
             // 
-            button4.Location = new Point(697, 57);
-            button4.Margin = new Padding(4, 5, 4, 5);
+            button4.Location = new Point(488, 34);
             button4.Name = "button4";
-            button4.Size = new Size(230, 80);
+            button4.Size = new Size(161, 48);
             button4.TabIndex = 4;
             button4.Text = "Dodaj zelenu povrsinu";
             button4.UseVisualStyleBackColor = true;
@@ -133,10 +95,9 @@
             // 
             // button5
             // 
-            button5.Location = new Point(697, 165);
-            button5.Margin = new Padding(4, 5, 4, 5);
+            button5.Location = new Point(488, 99);
             button5.Name = "button5";
-            button5.Size = new Size(230, 80);
+            button5.Size = new Size(161, 48);
             button5.TabIndex = 5;
             button5.Text = "Obrisi zelenu povrsinu";
             button5.UseVisualStyleBackColor = true;
@@ -144,16 +105,12 @@
             // 
             // ZelenePovrsineForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1034, 1107);
+            ClientSize = new Size(724, 637);
             Controls.Add(button5);
             Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
             Controls.Add(groupBox1);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "ZelenePovrsineForm";
             Text = "ZELENE POVRSINE";
             groupBox1.ResumeLayout(false);
@@ -168,9 +125,6 @@
         private ColumnHeader columnHeader2;
         private ColumnHeader columnHeader3;
         private ColumnHeader columnHeader4;
-        private Button button1;
-        private Button button2;
-        private Button button3;
         private Button button4;
         private Button button5;
     }
