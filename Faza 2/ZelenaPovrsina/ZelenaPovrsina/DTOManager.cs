d62fb73885ca8cb0f46d008e563edb261859d6f3
ricpe13 <pericpavle002@gmail.com>
2024-05-29 21:01:43 +0200

    Update DTOManager.cs
    Crveni se dosta

--- Faza 2/ZelenaPovrsina/ZelenaPovrsina/DTOManager.cs
+++ Faza 2/ZelenaPovrsina/ZelenaPovrsina/DTOManager.cs
@@ -8,5 +8,43 @@ namespace ZelenaPovrsina.DTO
 {
     internal class DTOManager
     {
+        #region Radnik
+        public static void dodajRadnika(RadnikBasic r)
+        {
+            try
+            {
+                ISession s = DataLayer.GetSession();
+
+                ZelenaPovrsina.Entiteti.Radnik o = new ZelenaPovrsina.Entiteti.Radnik();
+                o.Idr = r.Idr;
+                o.Ime = r.Ime;
+                o.Prezime = r.Prezime;
+                o.Jmbg = r.Jmbg;
+                o.Adresa = r.Adresa;
+                o.BrRadniKnjizice = r.BrRadniKnjizice;
+                o.ImeRoditelja = r.ImeRoditelja;
+                o.StrucnaSpema = r.StrucnaSpema;
+                o.DatumRodj = r.DatumRodj;
+                o.ZaZelenilo = r.ZaZelenilo;
+                o.ZaZelenilo = r.ZaZelenilo;
+                o.ZaHigijenu = r.ZaHigijenu;
+                o.ZaObjekat = r.ZaObjekat;
+                ZelenaPovrsina.Entiteti.GrupaRadnika gr = s.Load<ZelenaPovrsina.Entiteti.GrupaRadnika>(r.GrupaRadnika.IdG);
+                o.PripadaGrupi = gr;
+
+                s.SaveOrUpdate(o);
+
+
+
+                s.Flush();
+
+                s.Close();
+            }
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.FormatExceptionMessage());
+            }
+        }
+        #endregion
     }
 }
