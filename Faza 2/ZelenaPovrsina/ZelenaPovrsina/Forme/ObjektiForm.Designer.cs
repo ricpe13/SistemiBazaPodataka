923d3246c8e32425e7f9614ad48c55e045f42312
ricpe13 <pericpavle002@gmail.com>
2024-05-29 12:12:53 +0200

    ObjektiForm i KlupeForm

---
+++ Faza 2/ZelenaPovrsina/ZelenaPovrsina/Forme/ObjektiForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ZelenaPovrsina.Forme
+{
+    partial class ObjektiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(listView1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(280, 611);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Lista objekata";
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(6, 22);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(265, 583);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "ID parka";
+            columnHeader2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Redni broj";
+            columnHeader3.TextAlign = HorizontalAlignment.Center;
+            columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Tip";
+            columnHeader4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(330, 64);
+            button1.Name = "button1";
+            button1.Size = new Size(176, 36);
+            button1.TabIndex = 1;
+            button1.Text = "Dodaj objekat";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(330, 118);
+            button2.Name = "button2";
+            button2.Size = new Size(176, 33);
+            button2.TabIndex = 2;
+            button2.Text = "Obrisi objekat";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(330, 196);
+            button3.Name = "button3";
+            button3.Size = new Size(176, 45);
+            button3.TabIndex = 3;
+            button3.Text = "Klupe";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(330, 256);
+            button4.Name = "button4";
+            button4.Size = new Size(176, 38);
+            button4.TabIndex = 4;
+            button4.Text = "Drveca";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(330, 370);
+            button5.Name = "button5";
+            button5.Size = new Size(176, 39);
+            button5.TabIndex = 5;
+            button5.Text = "Skulpture";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(330, 310);
+            button6.Name = "button6";
+            button6.Size = new Size(176, 43);
+            button6.TabIndex = 6;
+            button6.Text = "Spomenici";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(330, 426);
+            button7.Name = "button7";
+            button7.Size = new Size(176, 42);
+            button7.TabIndex = 7;
+            button7.Text = "Fontane";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(330, 535);
+            button8.Name = "button8";
+            button8.Size = new Size(176, 34);
+            button8.TabIndex = 8;
+            button8.Text = "Svetiljke";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(330, 483);
+            button9.Name = "button9";
+            button9.Size = new Size(176, 37);
+            button9.TabIndex = 9;
+            button9.Text = "Decija igralista";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // ObjektiForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1049, 647);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Name = "ObjektiForm";
+            Text = "Objekti";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+    }
+}
